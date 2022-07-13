a82ebd03e5497594e772efb1683b790670b9f3c1
Jeromecda <jerome1.vasseur@yahoo.fr>
2022-07-13 16:24:26 +0200

    Punk Api final

--- punkapi/src/main/java/com/example/punkapi/MainActivity.kt
+++ punkapi/src/main/java/com/example/punkapi/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.punkapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import org.json.JSONArray
+import java.io.IOException
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val client = OkHttpClient()
+        val request  = Request.Builder().url("https://api.punkapi.com/v2/beers/random").build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(TAG, "onFailure: $e", )
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val stringBeer = response.body?.string()
+                val jsonArray = JSONArray(stringBeer)
+                val beerJson = jsonArray.getJSONObject(0)
+                Log.i(TAG, "onResponse: Nom de la bière : ${beerJson?.get("name")}")
+                Log.i(TAG, "onResponse: Degré d'alccol : ${beerJson?.get("abv")}")
+                Log.i(TAG, "onResponse: description : ${beerJson?.get("description")}")
+
+            }
+        })
     }
 }
